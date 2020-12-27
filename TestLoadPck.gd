0cd012b11920ddf2c148d28b4b62061d43348299
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-27 11:32:49 -0500

    set autoload

--- TestLoadPck.gd
+++ TestLoadPck.gd
@@ -11,7 +11,7 @@ const _FIRST_VERSION = "0.1.0"
 const _LAST_VERSION = "0.2.0"
  
 func _version():
-	return _version_by_file()
+	return _version_by_setting()
 
 func _version_by_setting():
 	return ProjectSettings.get(_VERSION_SETTING)
@@ -27,23 +27,27 @@ func _version_by_file():
 		return t
 		
 func _ready():
+	# init a version ProjectSetting
 	var pv = _version_by_setting()
 	if pv == null:
 		print("ProjectSettings version missing, setting it up for the first time")
-		ProjectSettings.set(_VERSION_SETTING, _FIRST_VERSION)
-		ProjectSettings.save()
+		_write_version_setting(_FIRST_VERSION)
 	else:
 		print("ProjectSettings %s: %s" % [ _VERSION_SETTING, _version_by_setting() ])
 	
+	# init a version file
 	var vf = File.new()
 	if !vf.file_exists(_VERSION_TXT):
 		_write_version_file(_FIRST_VERSION)
+
+
 	$WhatVersion.text = "This is v%s" % _version()
 
 func _on_WaitToLoad_timeout():
 	if _version() == _FIRST_VERSION:
 		ProjectSettings.load_resource_pack(_PCK)
-		print("Changing scene")
+		print("Still on the first version.  Changing scene")
+		_write_version_setting(_LAST_VERSION)
 		_write_version_file(_LAST_VERSION)
 		get_tree().change_scene("res://TestLoadPck.tscn")
 	else:
@@ -52,6 +56,11 @@ func _on_WaitToLoad_timeout():
 		var vd = Directory.new()
 		if vd.remove(_VERSION_TXT) != OK:
 			printerr("Failed to clean up %s" % _VERSION_TXT)
+
+func _write_version_setting(semver: String):
+	ProjectSettings.set(_VERSION_SETTING, _FIRST_VERSION)
+	ProjectSettings.save()
+	
 
 func _write_version_file(semver: String):
 	var vf = File.new()
