133d146f4b99fcdc7e6ba6fd497cb2e299b5f1d0
Terkwood <38859656+Terkwood@users.noreply.github.com>
2020-12-27 09:44:08 -0500

    Update the demo

--- TestLoadPck.gd
+++ TestLoadPck.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const _PCK = "test-0.2.0.pck"
 const _VERSION_TXT = "user://version.txt"
+const _FIRST_VERSION = "0.1.0"
+const _LAST_VERSION = "0.2.0"
  
 func _version():
 	var vf = File.new()
@@ -16,17 +18,21 @@ func _version():
 func _ready():
 	var vf = File.new()
 	if !vf.file_exists(_VERSION_TXT):
-		_write_version_file("0.1.0")
+		_write_version_file(_FIRST_VERSION)
 	$WhatVersion.text = "This is v%s" % _version()
 
 func _on_WaitToLoad_timeout():
-	if _version() == "0.1.0":
+	if _version() == _FIRST_VERSION:
 		ProjectSettings.load_resource_pack(_PCK)
 		print("Changing scene")
-		_write_version_file("0.2.0")
+		_write_version_file(_LAST_VERSION)
 		get_tree().change_scene("res://TestLoadPck.tscn")
 	else:
 		$Prompt.text = "All done."
+		# clean up the version file so we can run this demo again
+		var vd = Directory.new()
+		if vd.remove(_VERSION_TXT) != OK:
+			printerr("Failed to clean up %s" % _VERSION_TXT)
 
 func _write_version_file(semver: String):
 	var vf = File.new()
